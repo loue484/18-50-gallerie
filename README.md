d7ab156f35046cc7bf365058b2c838396025f5f7
Bruno Thomas <bruno@barreverte.fr>
2022-09-14 22:11:03 +0200

    feat: ajout styles et propriete des images

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 18-50-gallerie
+
+Pour installer les dépendances : 
+
+```shell
+$ npm ci
+```
+
+Pour générer `index.html`
+
+```shell
+$ npm run build
+```
+
--- genere_index.js
+++ genere_index.js
@@ -1,6 +1,17 @@
-const { readdir } = require('fs/promises');
+const { readdir, access, readFile } = require('fs/promises');
 const swig = require('swig');
+const sharp = require('sharp');
+const path = require('path');
 const template = swig.compileFile('index-template.html');
+
+async function exists (path) {
+    try {
+        await access(path)
+        return true
+    } catch {
+        return false
+    }
+}
 
 async function get_images(dir, ext) {
     const matchedFiles = [];
@@ -8,13 +19,23 @@ async function get_images(dir, ext) {
     const files = await readdir(dir);
 
     for (const file of files) {
-        if (file.endsWith(`.${ext}`)) {
-            matchedFiles.push(file);
+        if (file.endsWith(`.${ext}`) && !file.endsWith(`thumb.${ext}`)) {
+            const file_path =  path.join(dir, file)
+            let thumbnail_path = file_path.replace(ext, `thumb.${ext}`);
+            const photo = sharp(file_path)
+            if (! await exists(thumbnail_path)) {
+                await photo.resize({ height: 240 }).toFile(thumbnail_path);
+            }
+            const metadata = await photo.metadata()
+            metadata["path"] = file_path
+            metadata["thumbnailPath"] = thumbnail_path
+            matchedFiles.push(metadata)
         }
     }
     return matchedFiles;
 }
 
 (async function() {
-    console.log(template({images: await get_images('img', 'jpg')}));
+    const config = JSON.parse(await readFile('./properties.json', { encoding: 'utf8' }));
+    console.log(template({...config, images: await get_images('img', 'jpg')}));
 })();
