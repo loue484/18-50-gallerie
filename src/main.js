3cb5fb1dcf818997e5af6a30e2b9655e49992c00
Bruno Thomas <bruno@barreverte.fr>
2022-09-16 23:50:54 +0200

    refactor: ajout de la création de la distribution avec parcel

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import PhotoSwipeLightbox from '../node_modules/photoswipe/dist/photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#my-gallery',
+  children: 'a',
+  pswpModule: () => import('../node_modules/photoswipe/dist/photoswipe.esm.js')
+});
+lightbox.init();
